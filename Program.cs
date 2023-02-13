2c040c3b95f337dda17ea5b8c782646aa666047c
Sreelakshmi Sajeev <sreelakshmi.sajeev@experionglobal.local>
2023-02-13 15:06:36 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int a = 5, b = 10, temp;
+            Console.WriteLine("Before swap a= " + a + " b= " + b);
+            temp = a;
+            a = b;
+            b = temp;
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+}
